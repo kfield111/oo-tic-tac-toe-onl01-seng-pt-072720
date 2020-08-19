922e9cc139c93e6655be67cd21da892d698addbc
Kelin Field <kfield111@hotmail.com>
2020-08-19 23:43:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,7 +79,7 @@ x_positions = board.each_index.select{|i| board[i] == "X"}
 o_positions = board.each_index.select{|i| board[i] == "O"}
 
 WIN_COMBINATIONS.each do |combo|
-  if combo[0] == x_positions.find (combo) && combo[1] == x_positions.find (combo) && combo[2] == x_positions.find (combo)
+  if combo[0] == x_positions.find (combo[0]) && combo[1] == x_positions.find (combo[1]) && combo[2] == x_positions.find (combo[2])
     combo
   end
 
