40e5ac402c1903fab5fd56b5ed4d835ba0356af1
Kelin Field <kfield111@hotmail.com>
2020-08-20 00:04:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -80,6 +80,7 @@ o_positions = board.each_index.select{|i| board[i] == "O"}
 WIN_COMBINATIONS.each do |combo|
   x_positions & combo ? true : false}
 end
+end
 
 
 #INDEX:  0  1  2  3  4  5  6  7  8    --  DRAW
